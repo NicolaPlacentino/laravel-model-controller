c1b9c3472a41b0f9bf2c46f949ae1c456d53f441
NicolaPlacentino <nyko0921@gmail.com>
2023-02-24 17:11:09 +0100

    completed exercise

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -9,5 +9,5 @@ class Movie extends Model
 {
     use HasFactory;
 
-    private $timestamps = false;
+    public $timestamps = false;
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,18 @@
 @section('main-content')
     <div class="container py-3">
         <h1 class="mb-4">Movies</h1>
-        <section class="card-section">
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                  <h5 class="card-title">Title</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Original Title</h6>
-                  <p class="card-text mb-1">Nationality</p>
-                  <span class="card-link">date</span>
-                  <span class="card-link">vote</span>
+        <section class="card-section d-flex flex-wrap">
+            @foreach ($movies as $movie)
+                <div class="card m-3" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$movie->title}}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">{{$movie->original_title}}</h6>
+                        <p class="card-text mb-1">{{$movie->nationality}}</p>
+                        <span class="card-link">{{$movie->date}}</span>
+                        <span class="card-link">{{$movie->vote}}</span>
+                    </div>
                 </div>
-              </div>
+            @endforeach
         </section>
     </div>
 @endsection
